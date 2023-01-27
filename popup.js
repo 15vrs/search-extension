6bcfb3daee015dc64731675ebce541052ace5222
Vivian So <vivianrachelso@gmail.com>
2023-01-27 18:10:56 -0500

    read from pageCapture blob into string

--- popup.js
+++ popup.js
@@ -1,6 +1,39 @@
 chrome.tabs.query({ active: true, currentWindow: true }, ([tab]) => {
     chrome.pageCapture.saveAsMHTML({ tabId: tab.id }, (tabContentsBlob) => {
         console.log("iinside mhtml", tab.id);
+        const reader = tabContentsBlob.stream().getReader();
+        let charsReceived = 0;
+        let result;
+        reader.read().then(function processText({ done, value }) {
+          // Result objects contain two properties:
+          // done  - true if the stream has already given you all its data.
+          // value - some data. Always undefined when done is true.
+          if (done) {
+            console.log("Stream complete");
+            return;
+          }
+      
+          // // value for fetch streams is a Uint8Array
+          // charsReceived += value.length;
+          // const chunk = value;
+          // console.log(chunk)
+          // let listItem = document.createElement('li');
+          // listItem.textContent = `Received ${charsReceived} characters so far. Current chunk = ${chunk}`;
+          
+          // result += chunk;
+          
+          // convert stream into string?
+          let string = new TextDecoder().decode(value);
+          if (string.con)
+
+
+
+          // Read some more, and call this function again
+          return reader.read().then(processText);
+        });
+
+
+        console.log(reader);
         
         /**
          * download tab as html file
