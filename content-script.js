ad732e9f6b087c54547623d897b3331ab0cad172
Vivian So <vivianrachelso@gmail.com>
2023-03-14 17:44:25 -0400

    cleanup and refactor into functions

--- content-script.js
+++ content-script.js
@@ -1,19 +1,34 @@
 // list of chemicals to search for
 // source: https://davidsuzuki.org/living-green/dirty-dozen-cosmetic-chemicals-avoid/
 var chemicalsList = [
-    'BHA', 'butylated hydroxyanisole', 'BHT', 'butylated hydroxytulolene',
-    'p-phenylenediamine', 'CI', // coal tar dyes
-    'DEA', 'diethanolamine', 'MEA', 'monoethanolamide', 'TEA', 'triethanolamine',
-    'DBP', 'dibutyl phthalate', 'DEP', 'diethyl phthalate',
-    'menthenamine', 'quaternium-15', 'DMDM hydantoin', 'imidazolidinyl urea', 'sodium hydroxymethylglycinate', // formaldehyde-releasing preservatives
-    'paraben', 'methylparaben', 'butylparaben', 'propylparaben', 'isobutylparaben', 'ethylparaben',
-    'parfum', 'diethyl phthalate', 'DEP', //fragrance
-    'PEG', 'polyethylene glycol', 'propylene glycol', '1,4-dioxane',
-    'petrolatum',
-    'cyclotetrasiloxane', 'cylcopentasiloxane', 'cyclohexasiloxane', 'D4', 'D5', 'D6', 'cyclomethicone', 'polydimethylsiloxane', 'PDMS', 'dimethicone', // siloxanes
-    'sodium laureth sulfate', 'SLES', 'sodium lauryl sulfate', 'SLS',
-    'triclosan'
+    "BHA", "butylated hydroxyanisole", "BHT", "butylated hydroxytulolene",
+    "p-phenylenediamine", "CI", // coal tar dyes
+    "DEA", "diethanolamine", "MEA", "monoethanolamide", "TEA", "triethanolamine",
+    "DBP", "dibutyl phthalate", "DEP", "diethyl phthalate",
+    "menthenamine", "quaternium-15", "DMDM hydantoin", "imidazolidinyl urea", "sodium hydroxymethylglycinate", // formaldehyde-releasing preservatives
+    "paraben", "methylparaben", "butylparaben", "propylparaben", "isobutylparaben", "ethylparaben",
+    "parfum", "diethyl phthalate", "DEP", //fragrance
+    "PEG", "polyethylene glycol", "propylene glycol", "1,4-dioxane",
+    "petrolatum",
+    "cyclotetrasiloxane", "cylcopentasiloxane", "cyclohexasiloxane", "D4", "D5", "D6", "cyclomethicone", "polydimethylsiloxane", "PDMS", "dimethicone", // siloxanes
+    "sodium laureth sulfate", "SLES", "sodium lauryl sulfate", "SLS",
+    "triclosan"
 ]
+
+var dirtyDozen = {
+    "bha-bht": ["BHA", "butylated hydroxyanisole", "BHT", "butylated hydroxytulolene"],
+    "coal-tar-dyes": ["p-phenylenediamine", "CI"],
+    "dea": ["DEA", "diethanolamine", "MEA", "monoethanolamide", "TEA", "triethanolamine"],
+    "dibutyl-pthalates": ["DBP", "dibutyl phthalate", "DEP", "diethyl phthalate",],
+    "formaldehyde-preservetives": ["menthenamine", "quaternium-15", "DMDM hydantoin", "imidazolidinyl urea", "sodium hydroxymethylglycinate"],
+    "paraben": ["paraben", "methylparaben", "butylparaben", "propylparaben", "isobutylparaben", "ethylparaben"],
+    "fragrance": ["parfum", "diethyl phthalate", "DEP"],
+    "PEG": ["PEG", "polyethylene glycol", "propylene glycol", "1,4-dioxane"],
+    "petrolatum": ["petrolatum"],
+    "siloxane": ["cyclotetrasiloxane", "cylcopentasiloxane", "cyclohexasiloxane", "D4", "D5", "D6", "cyclomethicone", "polydimethylsiloxane", "PDMS", "dimethicone"],
+    "sodium-laureth-sulfate": ["sodium laureth sulfate", "SLES", "sodium lauryl sulfate", "SLS"],
+    "triclosan": ["triclosan"],
+}
 
 if (document.title.includes("Sephora")){
     searchSephora();
@@ -25,31 +40,53 @@ if (document.title.includes("Sephora")){
 }
 
 function searchSephora() {
-    var ingredients = document.getElementById("ingredients").getElementsByTagName("div")[1].innerHTML
+    var element = document?.getElementById("ingredients")?.getElementsByTagName("div")[1]?.innerHTML;
+    if (element){
+        var ingredients = findIngredients(element);
+        var ingredientsOfConcern = performSearch(ingredients);
+
+        // send message back to popup with ingredients that match
+        if (ingredientsOfConcern.length == 0) {
+            chrome.runtime.sendMessage({
+                sender: "sephora",
+                data: {
+                    ingredientsFound: false,
+                }
+            });
+        } else {
+            chrome.runtime.sendMessage({
+                sender: "sephora",
+                data: {
+                    ingredientsFound: true,
+                    ingredientsList: ingredientsOfConcern
+                }
+            });
+        }
+    } else {
+        chrome.runtime.sendMessage({
+            sender: "sephora",
+            data: "no product found"
+        });
+    }
+}
+
+function findIngredients(ingredients) {
     if (ingredients.includes("Clean at Sephora")) {
         ingredients = ingredients.substring(0, ingredients.indexOf("<br><br><b>Clean at Sephora"));
     }
-    
     // strip out intro blurb
-    if (ingredients.includes("<br>")){
-        ingredients = ingredients.substring(ingredients.lastIndexOf("<br>")+4, ingredients.length);
+    if (ingredients.includes("<br><br>")){
+        ingredients = ingredients.substring(ingredients.indexOf("<br><br>"), ingredients.length);
     }
-    // console.log("chemicals list \n", ingredients)
-    
-    // send message back to popup with ingredients that match
-    var ingredientsOfConcern = []
-    chemicalsList.forEach((chemical) => {
-        if (ingredients.includes(chemical)) {
-            ingredientsOfConcern.push(chemical)
-        }
-    })
-    console.log("ingredients of concern \n", ingredientsOfConcern)
-    chrome.runtime.sendMessage({
-        sender: "sephora",
-        data: {
-            ingredientsFound: true,
-            ingredientsList: ingredientsOfConcern
-        }
-    });
+    return ingredients;
 }
 
+function performSearch(ingredients) {
+    var ofConcern = [];
+    chemicalsList.forEach((chemical) => {
+        if (ingredients.includes(chemical)) {
+            ofConcern.push(chemical)
+        }
+    });
+    return ofConcern;
+}
