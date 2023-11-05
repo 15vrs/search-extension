bcb0668ae8be6eea527b863b23659a292eff2d8c
Vivian So <vivianrachelso@gmail.com>
2023-11-04 22:28:14 -0400

    add logo and icons

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,31 +1,35 @@
-const path = require('path');
-const CopyPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
 module.exports = {
-   mode: "production",
-   entry: {
-      background: path.resolve(__dirname, "..", "src", "background.ts"),
-      "content-script": path.resolve(__dirname, "..", "src", "content-script.ts"),
-      popup: path.resolve(__dirname, "..", "src", "popup.ts"),
-   },
-   output: {
-      path: path.join(__dirname, "../dist"),
-      filename: "[name].js",
-   },
-   resolve: {
-      extensions: [".ts", ".js"],
-   },
-   module: {
-      rules: [
-         {
-            test: /\.tsx?$/,
-            loader: "ts-loader",
-            exclude: /node_modules/,
-         },
-      ],
-   },
-   plugins: [
-      new CopyPlugin({
-         patterns: [{from: ".", to: ".", context: "public"}]
-      }),
-   ],
+  mode: "production",
+  entry: {
+    background: path.resolve(__dirname, "..", "src", "background.ts"),
+    "content-script": path.resolve(__dirname, "..", "src", "content-script.ts"),
+    popup: path.resolve(__dirname, "..", "src", "popup.ts"),
+  },
+  output: {
+    path: path.join(__dirname, "../dist"),
+    filename: "[name].js",
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+         test: /\.svg$/,
+         use: 'file-loader'
+       }
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: ".", to: ".", context: "public" }, { from: 'static' }],
+    }),
+  ],
 };
